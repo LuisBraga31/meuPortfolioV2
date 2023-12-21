bdfb00fb359226cf2cf2da259f488178a2b2059f
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-12-21 16:36:55 -0300

    Adicionando imagens e introdução na homepage

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,8 +1,26 @@
+import styles from './HomePage.module.css';
+import Perfil from '../../assets/perfil.png';
 
 export default function HomePage() {
 
     return (
         <main>
+            
+            <section id="introduction" className={styles.introduction}>
+            
+                <div className={styles.introductionText}>
+                    <div>
+                        <h1> Olá! Eu sou </h1>
+                        <h1> <strong className={styles.luisbragadev}> Luis Gustavo Braga</strong> </h1>
+                    </div>
+                    <p> Sou um jovem mineiro de 23 anos e desenvolvedor Front-end.</p>
+                </div>
+                
+                <div className={styles.introductionImage}>
+                    <img src={Perfil} alt="Luis Braga - perfil"/>
+                </div>
+
+            </section>
             
         </main>
     )
