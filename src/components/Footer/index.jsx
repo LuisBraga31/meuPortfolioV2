6e019ae2659aeec6c0f90f2d11b196823cda04a9
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-12-21 15:42:18 -0300

    Adicionando header e footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,6 +3,14 @@ export default function Footer() {
 
     return (
       <footer>
+
+        <div>
+
+        </div>
+
+        <div>
+          <small> © 2023 / Luis Braga Dev - Todos os direitos reservados </small>
+        </div>
           
       </footer>
     )
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,25 @@
+import styles from './Header.module.css';
 
 export default function Header() {
 
     return (
-      <header>
-          
+
+      <header id="header" className={styles.header}>
+        
+        <div className={styles.headerLogo}> 
+          <h2> Luis Braga <strong>Dev</strong> </h2>
+        </div>
+
+        <nav className={styles.headerNav}>
+          <a href="#"> Home </a>
+          <a href="#aboutme"> Sobre mim </a>
+          <a href="#skills"> Skills </a>
+          <a href="#projects"> Projetos </a>
+          <a href="#formations"> Formações </a>
+        </nav>
+
       </header>
+
     )
   
 }
