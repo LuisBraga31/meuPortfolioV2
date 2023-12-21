489b57c9a9c6b35f1eeb55e0d3dd91ab0998d556
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-12-20 21:45:16 -0300

    First configuration

---
+++ src/components/HomePage/index.jsx
@@ -0,0 +1,10 @@
+
+export default function HomePage() {
+
+    return (
+        <main>
+            
+        </main>
+    )
+
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Header from '../../components/Header';
+import HomePage from '../../components/HomePage';
+import Footer from '../../components/Footer';
+
+function Home() {
+
+  return (
+    <>
+      <Header/>
+      <HomePage/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
