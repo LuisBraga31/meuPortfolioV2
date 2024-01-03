1c679ec6b51398bb6d68401dacfa1340dc582a44
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-01-02 21:24:17 -0300

    Adicionando contatos ao footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,6 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+
 import styles from './Footer.module.css';
 
 export default function Footer() {
@@ -5,8 +8,23 @@ export default function Footer() {
     return (
       <footer>
 
-        <div>
+        <div className={styles.contacts}>
+          
+          <div className={styles.contactItem}>
+            <a href="https://github.com/LuisBraga31"> <FaGithub size={32}/> </a>
+            <p> github.com/LuisBraga31 </p>
+          </div>
 
+          <div className={styles.contactItem}>
+            <a href="https://www.linkedin.com/in/luis-gustavo-braga/"> <FaLinkedin size={32}/> </a>
+            <p> linkedin.com/in/luis-gustavo-braga/ </p>
+          </div>
+
+          <div className={styles.contactItem}>
+            <a href="mailto:luisgustavobsilva@gmail.com"> <BiLogoGmail size={32}/> </a>
+            <p> luisgustavobsilva@gmail.com </p>
+          </div>
+        
         </div>
 
         <div className={styles.footer}>
